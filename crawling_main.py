f81ef4b8b46f4a6349e671d29d2d4417c8e67064
je0ng3 <jepark2934@gmail.com>
2025-04-10 14:48:23 +0900

    [Chore] rename file

--- crawling_main.py
+++ crawling_main.py
@@ -5,7 +5,7 @@ print("안녕하세요! 데이터 수집 크롤러, ⭐️와구별⭐️ 입니
 while True:
     print("다운반고자 하는 데이터 번호를 입력해주세요.")
     print("-> 1. 질병 👿  2. 환경 🌱  3. 기후 🌞 (종료를 원하시면 0을 입력해주세요.)")
-    
+
     type = int(input("번호 입력: ").strip())
     if type > 3:
         print("❌ 올바른 숫자를 입력해주세요!")
@@ -19,5 +19,4 @@ while True:
     elif type == 2:
         subprocess.run(["python", "find_environment.py"])
     elif type == 3:
-        subprocess.run(["python", "find_weather.py"])
-
+        subprocess.run(["python", "find_climate.py"])
